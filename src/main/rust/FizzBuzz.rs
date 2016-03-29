b32d21b3a4cbf0b1ebc19e6227b291ebd2e107c2
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-03-29 10:01:05 +0100

    Add Rust implementation

---
+++ src/main/rust/FizzBuzz.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for i in 1..101 {
+        if i % 15 == 0 {
+            print!("FIZZBUZZ ");
+        } else if i % 3 == 0 {
+            print!("FIZZ ");
+        } else if i % 5 == 0 { 
+            print!("BUZZ ");
+        } else { 
+            print!("{} ", i);
+        }
+    }
+}
