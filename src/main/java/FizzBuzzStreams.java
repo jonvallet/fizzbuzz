455ca52d4031b3c4a70c5182433d546d37d2ceb2
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-04-05 14:10:14 +0100

    FizzBuzz using streams in java

---
+++ src/main/java/FizzBuzzStreams.java
@@ -0,0 +1,25 @@
+import java.util.stream.IntStream;
+
+/**
+ * Created by j.vallet@validus-ivc.co.uk on 05/04/16.
+ */
+public class FizzBuzzStreams {
+
+    public static String fizzbuzz(Integer e) {
+        if (e % 15 == 0) {
+            return "FIZZ";
+        } else if ( e % 3 == 0) {
+            return "BUZZ";
+        } else {
+            return e.toString();
+        }
+    }
+
+    public static void main (String [] args) {
+        IntStream stream = IntStream.range(1, 101);
+
+        stream.mapToObj(p -> fizzbuzz(p)).forEach( s -> System.out.print(s+" "));
+
+    }
+
+}
