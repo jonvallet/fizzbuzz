357bca39fdb02a9d2a527d9cbb1cb47c39d2eaa3
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-04-05 11:37:40 +0100

    Use a def for the fizzbuzz element instead of a closure

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,6 +1,8 @@
 object FizzBuzz {
+
+  def fizzbuzz(e: Int ): String =  if (e % 15 == 0) "FIZZBUZZ" else if (e % 3 == 0) "FIZZ" else if (e % 5 == 0) "BUZZ" else e.toString
+
   def main(args: Array[String]) = {
-   val list = (1 to 100).map(e => if (e % 15 == 0) "FIZZBUZZ" else if (e % 3 == 0) "FIZZ" else if (e % 5 == 0) "BUZZ" else e)
-   println(list.mkString(" "))
-  } 
+   println((1 to 100).map(fizzbuzz).mkString(" "))
+  }
 }
