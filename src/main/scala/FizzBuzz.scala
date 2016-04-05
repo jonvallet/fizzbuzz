2962500c66e7c66a9208a611becc96cfe0839528
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-04-05 11:40:54 +0100

    Use s formatter and some code reorganization

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,6 +1,9 @@
 object FizzBuzz {
 
-  def fizzbuzz(e: Int ): String =  if (e % 15 == 0) "FIZZBUZZ" else if (e % 3 == 0) "FIZZ" else if (e % 5 == 0) "BUZZ" else e.toString
+  def fizzbuzz(e: Int ): String =  if (e % 15 == 0) "FIZZBUZZ"
+    else if (e % 3 == 0) "FIZZ"
+    else if (e % 5 == 0) "BUZZ"
+    else s"$e"
 
   def main(args: Array[String]) = {
    println((1 to 100).map(fizzbuzz).mkString(" "))
