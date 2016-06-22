97f9cd063464ca5673350476229c1b9ff26c0f9e
j.vallet <j.vallet@validus-ivc.co.uk>
2016-06-22 16:22:20 +0100

    Kotlin FizzBuzz implementation

---
+++ src/main/kotlin/FizzBuzz.kt
@@ -0,0 +1,15 @@
+fun fizzbuzz(i: Int):String {
+	if (i % 15 == 0)
+		return "FizzBuzz"
+	else if (i % 3 == 0)
+		return "Fizz"
+	else if (i % 5 == 0)
+		return "Buzz"
+	else
+		return i.toString()
+}
+
+fun main (args: Array<String>) {
+        (1..100).map {fizzbuzz(it)}.forEach {print("$it ")}
+}
+
